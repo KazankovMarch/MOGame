cfdc47bcad57c4ee295f4432a318f1a5a484a67f
akazankov <akazankov@fix.ru>
2020-05-01 20:42:54 +0300

    SERVER gui for server

---
+++ MOGame-server/src/main/kotlin/ru/fixiki/mogame/core/GameServer.kt
@@ -0,0 +1,14 @@
+package ru.fixiki.mogame.core
+
+import java.io.File
+
+object GameServer {
+    fun start(get: File, text: String) {
+        Thread.sleep(2000)
+    }
+
+    fun stop() {
+        Thread.sleep(500)
+    }
+
+}
--- MOGame-server/src/main/kotlin/ru/fixiki/mogame/gui/views/MainView.kt
+++ MOGame-server/src/main/kotlin/ru/fixiki/mogame/gui/views/MainView.kt
@@ -1,26 +1,98 @@
 package ru.fixiki.mogame.gui.views
 
-import javafx.scene.control.Alert.AlertType.INFORMATION
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.control.Alert
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import ru.fixiki.mogame.core.GameServer
 import ru.fixiki.mogame.gui.Styles
 import tornadofx.*
+import java.io.File
 
-class MainView : View("Hello TornadoFX") {
+class MainView : View("MOGame Server") {
+    private var gamePackageFile = SimpleObjectProperty<File>()
+    private var portProperty = SimpleStringProperty()
+    private var startButton: Button by singleAssign()
+    private var stopButton: Button by singleAssign()
+    private var label: Label by singleAssign()
+
     override val root = borderpane {
         addClass(Styles.welcomeScreen)
         top {
             stackpane {
-                label(title).addClass(Styles.heading)
+                label = label(title).addClass(Styles.heading)
             }
         }
         center {
-            stackpane {
-                addClass(Styles.content)
-                button("Click me") {
-                    setOnAction {
-                        alert(INFORMATION, "Well done!", "You clicked me!")
+            addClass(Styles.content)
+            vbox {
+                hbox {
+                    label("Port for server")
+                    textfield {
+                        filterInput { it.controlNewText.isInt() }
+                    }.textProperty().bindBidirectional(portProperty)
+                }
+                hbox {
+                    button {
+                        text = "Select Game Package"
+                        action {
+                            gamePackageFile.set(chooseFile("", arrayOf()).firstOrNull())
+                        }
+                    }
+                    startButton = button {
+                        text = "Start"
+                        action {
+                            if (label.text != "MOGame Server") return@action
+
+                            val errors = validateData()
+                            if (errors.isEmpty()) {
+                                label.text = "Starting..."
+                                runAsyncWithProgress {
+                                    GameServer.start(gamePackageFile.get(), portProperty.get())
+                                }.ui {
+                                    isDisable = true
+                                    stopButton.isDisable = false
+                                    label.text = "MOGame Server (Launched)"
+                                }
+                            } else {
+                                alert(
+                                        type = Alert.AlertType.WARNING,
+                                        header = "Can't start server",
+                                        content = errors.joinToString()
+                                )
+                            }
+                        }
+                    }
+                    stopButton = button {
+                        text = "Stop"
+                        isDisable = true
+                        action {
+                            label.text = "Stopping..."
+                            isDisable = true
+                            runAsync {
+                                GameServer.stop()
+                            }.ui {
+                                label.text = "MOGame Server"
+                                startButton.isDisable = false
+                            }
+                        }
                     }
                 }
+
             }
         }
     }
+
+    private fun validateData(): List<String> {
+        val errors = ArrayList<String>()
+        if (gamePackageFile.get() == null) {
+            errors += "Game package not selected"
+        }
+        if (portProperty.get() == null) {
+            errors += "Server port not specified"
+        }
+        return errors
+    }
+
 }
